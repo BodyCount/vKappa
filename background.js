e6a95df12f040d28339589dd7c31051bcdd196e6
BodyCount <jon1887@gmail.com>
2015-10-07 14:46:05 +0300

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tabId, info, tab){
-    if(info && info.status === "complete"){
-        chrome.tabs.sendMessage(tabId, {data: tab}, function(response) {
-
-        });
-
-    }
-});
